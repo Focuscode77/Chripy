6e95ac045256fd38e486d1fa1a8205aefb40c937
Joshua Coleman <procoleman77@outlook.com>
2019-11-17 18:27:39 -0500

    Working chrip completed

--- app/public/js/chirp.js
+++ app/public/js/chirp.js
@@ -1,64 +1,57 @@
 // golbal moment
-
-
-$("#chirp-submit"),on("click", function(event){
-event.perventDefault();
-
-// Make newChrip object
-var newChrip = {
+$("#chirp-submit").on("click", function(event) {
+    event.preventDefault();
   
-    author: $("#author").val().trim(),
-    body: $("#chirp-box").val().trim(),
-    created_at: moment().format("YYYY-MM-DD HH:mm:ss")
-};
-
-console.log(newChrip);
-
-// Send an AJAX
-$.post("/api/new", newChrip)
-
-.then(function(){
-
-var row = $("<div>");
-row.addClass("chrip");
-
-row.append("<p>" + newChrip.author + " chirped: </p>");
-row.append("<p>" + newChrip.body + "</p>");
-row.append("<p>At " + moment(newChirp.created_at).format("h:mma on dddd") + "</p>");
-
-$("#chirp-area").prepend(row);
-
-});
-
-$("author").val("");
-$("#chirp-box").val("");
-
-
-});
-
-// Grabs all your chirps
-
-$.get("/api/all", function(data){
-
-if(data.length !== 0){
-
-    for(var i =0; i < data.length; i++){
-
-     var row = $("<div>");
-     row.addClass("chrip");
-
-
-     row.append("<p>" + data[i].author + " chirped.. </p>");
-     row.append("<p>" + data[i].body + "</p>");
-     row.append("<p>At " + moment(data[i].created_at).format("h:mma on dddd") + "</p>");
-
-     $("#chirp-area").prepend(row);
-
-
+    // Make a newChirp object
+    var newChirp = {
+      author: $("#author").val().trim(),
+      body: $("#chirp-box").val().trim(),
+      created_at: moment().format("YYYY-MM-DD HH:mm:ss")
+    };
+  
+    console.log(newChirp);
+  
+    // Send an AJAX POST-request with jQuery
+    $.post("/api/new", newChirp)
+      // On success, run the following code
+      .then(function() {
+  
+        var row = $("<div>");
+        row.addClass("chirp");
+  
+        row.append("<p>" + newChirp.author + " chirped: </p>");
+        row.append("<p>" + newChirp.body + "</p>");
+        row.append("<p>At " + moment(newChirp.created_at).format("h:mma on dddd") + "</p>");
+  
+        $("#chirp-area").prepend(row);
+  
+      });
+  
+    // Empty each input box by replacing the value with an empty string
+    $("#author").val("");
+    $("#chirp-box").val("");
+  });
+  
+  // When the page loads, grab all of our chirps
+  $.get("/api/all", function(data) {
+  
+    if (data.length !== 0) {
+  
+      for (var i = 0; i < data.length; i++) {
+  
+        var row = $("<div>");
+        row.addClass("chirp");
+  
+        row.append("<p>" + data[i].author + " chirped.. </p>");
+        row.append("<p>" + data[i].body + "</p>");
+        row.append("<p>At " + moment(data[i].created_at).format("h:mma on dddd") + "</p>");
+  
+        $("#chirp-area").prepend(row);
+  
+      }
+  
     }
+  
+  });
+  
 
-
-}
-
-
-});
--- server.js
+++ server.js
@@ -25,5 +25,5 @@ app.listen(PORT, function(){
 console.log("App listening on PORT" + PORT);
 
 
-})
+});
 
